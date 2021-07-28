d37b181a8dac58427ce09b5752e13d1dcae15e68
PrajwalKarale08 <87708447+PrajwalKarale08@users.noreply.github.com>
2021-07-28 19:22:28 +0530

    Added new app for user controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import SystemStatusController from './components/system-status/system-status.controller';
+import UserController from './components/user/user';
 
 export default function registerRoutes(app: express.Application): void {
+    new UserController(app);
     new SystemStatusController(app);
 }
